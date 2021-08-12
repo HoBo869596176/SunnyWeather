7c0617f4aeeb0121a8eeb33d915ea0f5ea4f8c42
hebao <app123456>
2021-08-12 17:32:14 +0800

    提交

---
+++ app/src/main/java/com/sunnyweather/demo/Test2Activity.java
@@ -0,0 +1,7 @@
+package com.sunnyweather.demo;
+
+import android.app.Application;
+
+public class Test2Activity extends Application {
+11111111
+}
---
+++ app/src/main/java/com/sunnyweather/demo/TestActivity.java
@@ -0,0 +1,4 @@
+package com.sunnyweather.demo;
+
+public class TestActivity {
+}
